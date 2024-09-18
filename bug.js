ec6523a66bcd29ef87f26dc550ce3a046ffc2247
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-09-18 10:02:52 +0530

    find bug in js

---
+++ bug.js
@@ -0,0 +1,9 @@
+var numOne = "1";
+var numTwo = "2";
+
+console.log(numOne + numTwo); // That mean merge two String
+
+console.log(numOne - numTwo); // But this is bug...
+
+console.log();
+
---
+++ index.js
@@ -0,0 +1,6 @@
+var num = 15;
+var newNum = num++;
+var fnlNum = newNum++;
+console.log(num);
+console.log(newNum);
+console.log(fnlNum);
