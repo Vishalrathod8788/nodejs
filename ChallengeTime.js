62d23de27cebfa96b8af4210d5d70d3f1c94d473
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-09-24 09:58:57 +0530

    Write Swap Program

--- ChallengeTime.js
+++ ChallengeTime.js
@@ -1,9 +1,28 @@
 //--> Operatore
 
-// What will be the output of 3**3?
+// 1). What will be the output of 3**3?
 
 // console.log(3**3);  // 27
 
-// What will be the output, when we add a number and string?
+// 2). What will be the output, when we add a number and string?
 
 // console.log(1 + "1");  // 11
+
+// 3). Write a program to swap two numbers?
+
+// let a = 5;
+// let b = 10;
+
+// // 1st Method
+// [a, b] = [b, a];
+
+// console.log("a = " + a, "b = " + b);
+
+// 2nd Method
+// console.log("a = " +a, "b = "+b);
+
+// let temp = a;
+// a = b;
+// b = temp;
+
+// console.log("a = " +a, "b = "+b);
