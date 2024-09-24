29a55f37395a3da375ca07ac4a4cd423dcf4fdd3
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-09-24 09:47:23 +0530

    code add

--- bug.js
+++ bug.js
@@ -1,9 +1,17 @@
 var numOne = "1";
 var numTwo = "2";
 
-console.log(numOne + numTwo); // That mean merge two String
+// console.log(numOne + numTwo); // That mean merge two String
 
-console.log(numOne - numTwo); // But this is bug...
+// console.log(numOne - numTwo); // But this is bug...
 
-console.log();
+// console.log(numOne / numTwo);
 
+// console.log('-5' - true); //Output 4
+
+// var a = [1, 2, 3];
+// var b = a;
+// b.push(4);
+// console.log(a);  // Output [1, 2, 3, 4]
+
+console.log("7" + 7 == "7" + 7);
