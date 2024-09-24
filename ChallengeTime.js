fdea065124df6c5215a80cd39799105b3c185694
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-09-24 09:52:40 +0530

    Add New File

---
+++ ChallengeTime.js
@@ -0,0 +1,9 @@
+//--> Operatore
+
+// What will be the output of 3**3?
+
+// console.log(3**3);  // 27
+
+// What will be the output, when we add a number and string?
+
+// console.log(1 + "1");  // 11
