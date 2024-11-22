601fa385d1d260e10bb8aa5dca8e164000bbd351
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-22 17:50:53 +0530

    fat arrow function write

--- Learn_Node/app.js
+++ Learn_Node/app.js
@@ -1,9 +1,5 @@
 console.log("Hello World");
 
-let arr = [1, 2, 3, 4, 5];
+const arr = [1, 2, 3, 4, 5];
 
-let ans = arr.map((num) => {
-  num * num;
-});
-
-console.log(ans);
+console.log(arr.map((num) => num * num));
