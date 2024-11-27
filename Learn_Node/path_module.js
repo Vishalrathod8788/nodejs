42bbfbaf775bc031237e08d1b0d02468c6b40490
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-27 16:54:37 +0530

    basename & dirname

--- Learn_Node/path_module.js
+++ Learn_Node/path_module.js
@@ -6,4 +6,6 @@ const filePath = path.join("Folder", "Student", "Data.txt");
 console.log(filePath);
 console.log(path.parse(filePath));
 console.log(path.resolve("Folder", "Student", "Data.txt"));
+console.log(path.basename(filePath));
+console.log(path.dirname(filePath));
 
