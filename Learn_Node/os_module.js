e3f6d60f33e49c79d7c53edc52f584e22d180fcf
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-27 17:02:30 +0530

    os.freemem

--- Learn_Node/os_module.js
+++ Learn_Node/os_module.js
@@ -7,3 +7,7 @@ const os = require("os");
 // 1. os.arch()
 // Returns the operating system CPU architecture
 console.log("OS CPU architecture: " + os.arch());
+
+// 2. os.cpus()
+// Returns an array of objects containing information about each CPU/core
+console.log("Free memory: " + os.freemem());
