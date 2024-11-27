3daf11e2c3c62248ce218cd4d82b91ae47108553
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-27 17:01:43 +0530

    os.arch

---
+++ Learn_Node/os_module.js
@@ -0,0 +1,9 @@
+// Os Module in Node.js
+// The OS module provides operating system-related utility methods and properties.
+// It can be accessed using:
+
+const os = require("os");
+
+// 1. os.arch()
+// Returns the operating system CPU architecture
+console.log("OS CPU architecture: " + os.arch());
