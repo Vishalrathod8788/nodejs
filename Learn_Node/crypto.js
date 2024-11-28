9101bb2ee989ae5df828b72c3959dc26404d8f82
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-28 17:00:55 +0530

    timing safe equal

--- Learn_Node/crypto.js
+++ Learn_Node/crypto.js
@@ -13,3 +13,8 @@ console.log(randomBytes.toString("hex"));
 // randomInt
 const randomInt = crypto.randomInt(10, 100);
 console.log(randomInt);
+
+// timingSafeEqual
+const buffer1 = Buffer.from("Hello, World!");
+const buffer2 = Buffer.from("Hello, World!");
+console.log(crypto.timingSafeEqual(buffer1, buffer2));
