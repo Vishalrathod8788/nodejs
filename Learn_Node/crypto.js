a6a6434a4d71816abc2c5ac0bc759d09c8f5c4d0
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-28 17:00:11 +0530

    createhashnode

--- Learn_Node/crypto.js
+++ Learn_Node/crypto.js
@@ -1,2 +1,7 @@
 // crypto module in Node.js
 const crypto = require("crypto");
+
+// createhash node
+const hash = crypto.createHash("sha256");
+hash.update("Hello, World!");
+console.log(hash.digest("hex"));
