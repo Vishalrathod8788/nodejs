e1550fd96ba567132b003505179e6f7f1019b969
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-29 16:18:29 +0530

    fs module import and code write

--- Learn_Node/fs/syncfs.js
+++ Learn_Node/fs/syncfs.js
@@ -1,4 +1,12 @@
-
+// utf-8 (8-bit Unicode Transformation Format) --> Encode character from nearly all written language
 const fs = require("fs");
 
-fs.readFileSync
+const fileName = "test.txt";
+
+const writefile = fs.readFileSync(
+  fileName,
+  "This is the initial comment",
+  "utf-8"
+);
+
+console.log(writefile);
