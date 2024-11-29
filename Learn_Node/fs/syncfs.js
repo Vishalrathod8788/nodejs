041a0e464d170cca2ec7962996f4c7ced8a8379f
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-29 16:36:33 +0530

    Write, read , update and delete operation code write

--- Learn_Node/fs/syncfs.js
+++ Learn_Node/fs/syncfs.js
@@ -3,10 +3,25 @@ const fs = require("fs");
 
 const fileName = "test.txt";
 
-const writefile = fs.readFileSync(
-  fileName,
-  "This is the initial comment",
-  "utf-8"
-);
+// const writefile = fs.writeFileSync(
+//   fileName,
+//   "This is the initial comment",
+//   "utf-8"
+// );
 
-console.log(writefile);
+// console.log(writefile);
+
+// Read File
+
+// const readfile = fs.readFileSync(fileName, "utf-8");
+// console.log("Read :", readfile);
+
+// Update File
+// const updateFile = fs.appendFileSync(fileName, "This is the updated comment");
+// console.log(updateFile);
+// const readfile2 = fs.readFileSync(fileName, "utf-8");
+// console.log("Update :", readfile2);
+
+// fs.unlink
+// const deleteFile = fs.unlinkSync(fileName);
+// console.log(deleteFile);
