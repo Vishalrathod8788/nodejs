5dff9fa64ddd31545aadf6368f61a726fb155bd7
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-12-01 18:18:49 +0530

    Enhance fspromises.js: Update directory read output, implement file write with error logging, and clean up commented-out code

--- Learn_Node/fs/fspromises.js
+++ Learn_Node/fs/fspromises.js
@@ -1,4 +1,3 @@
-const { error, log } = require("console");
 const fs = require("fs");
 const path = require("path");
 
@@ -10,20 +9,32 @@ const filePath = path.join(__dirname, fileName);
 
 fs.promises
   .readdir(dirName)
-  .then((data) => console.log(data))
+  .then((data) => console.log("Read a Directory :", data))
   .catch((error) => console.log(error));
 
+const updateData = "new update File";
 fs.promises
-  .readFile(fileName, "utf-8")
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error));
+  .writeFile(fileName, updateData, "utf-8")
+  .then(() => console.log("File written successfully:", updateData))
+  .catch((error) => {
+    // Write error to error.txt file
+    const errorMessage = `${new Date().toISOString()} - Error: ${
+      error.message
+    }\n`;
+    fs.promises
+      .appendFile("error.txt", errorMessage, "utf-8")
+      .then(() => console.log("Error logged to file"))
+      .catch((err) => console.log("Failed to write error log:", err));
 
-fs.promises
-  .writeFile(fileName, "update File", "utf-8")
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error));
+    console.log(error);
+  });
 
-fs.promises
-  .unlink(fileName)
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error));
+// fs.promises
+//   .readFile(fileName, "utf-8")
+//   .then((data) => console.log("Read a File :", data))
+//   .catch((error) => console.log(error));
+
+// fs.promises
+//   .unlink(fileName)
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
