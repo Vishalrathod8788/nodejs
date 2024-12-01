41d07db4eff2b16ba888d926bf5ed6d9d636b9a9
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-12-01 17:44:06 +0530

    Remove fsPromises.js file: eliminate unused file handling code for improved project clarity

--- Learn_Node/fs/fspromises.js
+++ Learn_Node/fs/fspromises.js
@@ -1,13 +1,13 @@
-const { error } = require("console");
 const fs = require("fs");
 const path = require("path");
 
 const fileName = "fsPromises.txt";
+const dirName = __dirname;
 const filePath = path.join(__dirname, fileName);
 
 // why .then() and .catch() explain below
 
 fs.promises
-  .readdir(filePath)
+  .readdir(dirName)
   .then((data) => console.log(data))
   .catch((error) => console.log(error));
