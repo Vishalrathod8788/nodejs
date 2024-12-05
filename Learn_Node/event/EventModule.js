217a81032951b47e838a342dee912a1a2d3365b7
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-05 10:17:22 +0530

    Refactor EventModule.js: Update greet event listener examples to demonstrate multiple ways of handling events, enhancing clarity and usability of the event emitter functionality.

--- Learn_Node/event/EventModule.js
+++ Learn_Node/event/EventModule.js
@@ -8,18 +8,21 @@ const emitter = new EventEmitter();
 //1. Define an EventListner (addEvent Listner)
 //2. Trigger (emit) the greet event
 
+// first way ->
 // emitter.on("greet", () => {
 //   console.log("Hello, Vishal Rathod");
 // });
 
 // emitter.emit("greet");
 
+// Second way ->
 // emitter.on("greet", (name) => {
 //   console.log(`Hello, ${name}`);
 // });
 
 // emitter.emit("greet", "Vishal Rathod");
 
+// Thired way
 emitter.on("greet", (args) => {
   console.log(`Hey! ${args.name} You are a ${args.prof}`);
 });
