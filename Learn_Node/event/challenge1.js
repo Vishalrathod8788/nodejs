8a196f22ca7a97e2317d233fcdf79139a0a608ff
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-05 12:25:10 +0530

    Implement event tracking and persistence in challenge1.js: Introduce custom event handling with user-login, user-logout, user-purchased, and profile-update events. Add functionality to read and write event counts to eventCount.json, ensuring data persistence across sessions.

--- Learn_Node/event/challenge1.js
+++ Learn_Node/event/challenge1.js
@@ -1,37 +1,49 @@
+const { log } = require("console");
 const emitter = require("events");
+const fs = require("fs");
 
 // create at least four custom events (e. g., user-login, user-logout, user-purchase, profile-update)
 // Emit this event multiple time with diffrent argument (e. g., username, item purchased).
 // Treack and store the count of each event type:
 // Define summury event that logs a detailed report of how many time each event was triggered.
 
-const eventCount = {
-  "user-login": 0,
-  "user-logout": 0,
-  "user-purchased": 0,
-  "profile-update": 0,
-};
+let eventCount;
+try {
+  const data = fs.readFileSync("eventCount.json");
+  eventCount = JSON.parse(data);
+} catch (error) {
+  eventCount = {
+    "user-login": 0,
+    "user-logout": 0,
+    "user-purchased": 0,
+    "profile-update": 0,
+  };
+}
 
 const myEmitter = new emitter();
 
 myEmitter.on("log-in", (username) => {
   console.log(`${username} wants to login, please `);
   eventCount["user-login"] += 1;
+  fs.writeFileSync("eventCount.json", JSON.stringify(eventCount));
 });
 
 myEmitter.on("log-out", (username) => {
   console.log(`${username} wants to logout, please`);
   eventCount["user-logout"] += 1;
+  fs.writeFileSync("eventCount.json", JSON.stringify(eventCount));
 });
 
 myEmitter.on("user-purchased", (product) => {
   console.log(`${product} purchaed !`);
   eventCount["user-purchased"] += 1;
+  fs.writeFileSync("eventCount.json", JSON.stringify(eventCount));
 });
 
 myEmitter.on("profile-update", (email) => {
   console.log(`${email} updated !`);
   eventCount["profile-update"] += 1;
+  fs.writeFileSync("eventCount.json", JSON.stringify(eventCount));
 });
 
 myEmitter.on("summary", () => {
