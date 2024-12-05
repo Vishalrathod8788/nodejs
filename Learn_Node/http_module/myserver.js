25d843750ab1f7e42921d8c9d4b6f8c59e2ee6bc
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-05 22:51:05 +0530

    Consolidate file handling improvements in fspromises.js: Enhance read, write, and delete operations with better error logging and cleanup. Update event handling examples in EventModule.js for clarity and usability. Remove unused code to streamline the project.

---
+++ Learn_Node/http_module/myserver.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+const myServer = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Welcome to our home page");
+  }
+
+  if (req.url === "/about") {
+    res.end("Welcome to our about page");
+  }
+
+  if (req.url === "/contact") {
+    res.write("Welcome to our contact page");
+    res.end();
+  }
+});
+
+const PORT = 3000;
+
+myServer.listen(PORT, () => {
+  console.log(`Server Started PORT on ${PORT}`);
+});
