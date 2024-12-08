bc590c4581932a884f9ba0641a89e9ed931b4e32
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-08 16:39:37 +0530

    Consolidate event handling and file operations: Enhance error logging and cleanup in fspromises.js, implement async file writing in fsAsyncAwait.js, and improve event listener examples in EventModule.js. Update server response messages in myserver.js for better clarity and introduce event tracking in challenge1.js to ensure data persistence across sessions.

---
+++ Learn_Node/es_module/app.js
@@ -0,0 +1,18 @@
+// import module using common js
+// const math = require("./math");
+
+// import module using es6
+import mult from "./math.js";
+import sub from "./math.js";
+
+// console.log(`Addition:`, math.add(5, 10));
+
+// console.log(`Subtraction:`, math.sub(5, 10));
+
+console.log(`Multuplication:`, mult(5, 10));
+
+// console.log(`Division:`, math.div(5, 10));
+
+// console.log(`PI:`, math.PI);
+
+// this is common JS
