7340a531b2015572b8cd09c69e0ad2a39d3fec9d
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-15 11:45:59 +0530

    comment code is the common JS code so but My Confuguration file in module type so this code will error occure

--- Learn_Node/app.js
+++ Learn_Node/app.js
@@ -1,13 +1,13 @@
-const math = require("./math");
+// const math = require("./math");
 
-console.log(`Addition:`, math.add(5, 10));
+// console.log(`Addition:`, math.add(5, 10));
 
-console.log(`Subtraction:`, math.sub(5, 10));
+// console.log(`Subtraction:`, math.sub(5, 10));
 
-console.log(`Multuplication:`, math.mult(5, 10));
+// console.log(`Multuplication:`, math.mult(5, 10));
 
-console.log(`Division:`, math.div(5, 10));
+// console.log(`Division:`, math.div(5, 10));
 
-console.log(`PI:`, math.PI);
+// console.log(`PI:`, math.PI);
 
 // this is common JS
