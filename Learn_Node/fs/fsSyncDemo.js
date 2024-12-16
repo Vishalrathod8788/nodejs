92fbd21e3fb4bf3c58f4495c7436d4b8e1dbc660
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-16 13:07:39 +0530

    Comment out all code in fsSyncDemo.js to prevent execution errors due to module type configuration. This change ensures compatibility with the current project setup.

--- Learn_Node/fs/fsSyncDemo.js
+++ Learn_Node/fs/fsSyncDemo.js
@@ -1,13 +1,13 @@
-import fs from "fs";
-console.log("start");
+// import fs from "fs";
+// console.log("start");
 
-// Set a delayed message using setTimeout
-setTimeout(() => {
-  console.log("Hey! How are you!");
-}, 2000);
+// // Set a delayed message using setTimeout
+// setTimeout(() => {
+//   console.log("Hey! How are you!");
+// }, 2000);
 
-// Read file asynchronously and handle potential errors
-const data = fs.readFileSync("error.txt", "utf-8");
-console.log(data);
+// // Read file asynchronously and handle potential errors
+// const data = fs.readFileSync("error.txt", "utf-8");
+// console.log(data);
 
-console.log("End");
+// console.log("End");
