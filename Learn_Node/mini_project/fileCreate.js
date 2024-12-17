d507fab677e4e176175c7161a35da5204a4603d6
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-17 17:04:24 +0530

    Refactor app.js for improved task management and user feedback: Enhanced input handling for task addition, updated user prompts for clarity, and improved task display logic to handle empty lists. This update aims to streamline user interactions and provide consistent feedback throughout the task management process.

---
+++ Learn_Node/mini_project/fileCreate.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter File Name : ", (Fname) => {
+  rl.question("Enter File Contain :", (Fcontain) => {
+    fs.writeFile(`${Fname}.txt`, `${Fcontain}`, "utf-8", (error) => {
+      if (error) {
+        console.log("Error Occured");
+      } else {
+        console.log(`${Fname}.txt File Created Successfully`);
+      }
+    });
+  });
+});
