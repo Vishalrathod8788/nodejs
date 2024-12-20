8f669092c69bdc11407dbef2849223aad52cd95a
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-20 22:16:54 +0530

    Add basic arithmetic operations and PI constant to app.js: Implemented addition, subtraction, multiplication, and division functions from the math module, enhancing the application's mathematical capabilities.

--- Learn_Node/app.js
+++ Learn_Node/app.js
@@ -11,3 +11,15 @@
 // console.log(`PI:`, math.PI);
 
 // this is common JS
+
+const { add, sub, mult, div, PI } = require("./math");
+
+console.log(`Addition:`, add(5, 10));
+
+console.log(`Subtraction:`, sub(5, 10));
+
+console.log(`Multuplication:`, mult(5, 10));
+
+console.log(`Division:`, div(5, 10));
+
+console.log(`PI:`, PI);
