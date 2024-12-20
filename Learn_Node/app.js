47d1f117cf232204e0cfb8f871583b662358ce81
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-20 22:17:16 +0530

    Remove console logs for arithmetic operations in app.js: Cleaned up the code by removing direct console log statements for addition, subtraction, multiplication, division, and PI, streamlining the application logic.

--- Learn_Node/app.js
+++ Learn_Node/app.js
@@ -13,13 +13,3 @@
 // this is common JS
 
 const { add, sub, mult, div, PI } = require("./math");
-
-console.log(`Addition:`, add(5, 10));
-
-console.log(`Subtraction:`, sub(5, 10));
-
-console.log(`Multuplication:`, mult(5, 10));
-
-console.log(`Division:`, div(5, 10));
-
-console.log(`PI:`, PI);
