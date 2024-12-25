2bcf98737c4f2b5b1e69088cbe456e71465ea93d
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-25 17:38:23 +0530

    Fix file reading in URL shortener: Update paths for HTML and CSS files and correct response handling for 404 errors.

--- Learn_Node/mini_project/url/app.js
+++ Learn_Node/mini_project/url/app.js
@@ -1,14 +1,28 @@
 import { createServer, get } from "http";
 import { fs } from "fs/promises";
+import { path } from "path";
 
 const server = createServer(async (req, res) => {
   if (req.method === "GET") {
     if (req.url === "/") {
       try {
-        const data = await fs.reasFile("./index.html");
+        const data = await fs.readFile(path.join("public", "index.html"));
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
       } catch (error) {
         res.writeHead(404, { "Content-Type": "text/plain" });
         req.end("404 Page Not Found");
+      }
+    } else if (req.method === "GET") {
+      if (req.url === "/") {
+        try {
+          const data = await fs.readFile(path.join("public", "style.css"));
+          req.writeHead(200, { "Content-Type": "text/css" });
+          res.end(data);
+        } catch (error) {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          req.end("404 Page Not Found");
+        }
       }
     }
   }
