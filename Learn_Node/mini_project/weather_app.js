7236f49633f0cdd609f50f7b118d41452d4bfa38
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-25 12:11:02 +0530

    Refactor weather app to use HTTP module for API calls and add URL shortener HTML template

--- Learn_Node/mini_project/weather_app.js
+++ Learn_Node/mini_project/weather_app.js
@@ -7,22 +7,46 @@
 import readline from "readline";
 import chalk from "chalk";
 
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-rl.question(chalk.blue("Enter the city name: "), (city) => { // async function
-    try {
-        const response = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2e2f6b3f9e6e4c7c3d3b7b6e7b7b6e7`);
-        console.log(chalk.green(`The weather of ${city} is ${response.data.weather[0].description}`));
-    } catch (error) {
+rl.question(chalk.blue("Enter the city name: "), (city) => {
+  // async function
+  try {
+    https
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2e2f6b3f9e6e4c7c3d3b7b6e7b7b6e7`,
+        (response) => {
+          console.log(response);
+
+          let data = "";
+          response.on("data", (chunk) => {
+            data += chunk;
+          });
+          response.on("end", () => {
+            const weatherData = JSON.parse(data);
+            console.log(
+              chalk.green(
+                `The weather of ${city} is ${weatherData.weather[0].description}`
+              )
+            );
+            rl.close();
+          });
+        }
+      )
+      .on("error", (error) => {
         console.log(chalk.red("City not found!"));
-    }
+        rl.close();
+      });
+  } catch (error) {
+    console.log(chalk.red("City not found!"));
     rl.close();
-}
-);
-
+  }
+});
 rl.on("close", () => {
-    console.log(chalk.yellow("Thanks for using our weather app!"));
-}
-);
+  console.log(chalk.yellow("Thanks for using our weather app!"));
+});
 
 // Run this file using node weather_app.js
