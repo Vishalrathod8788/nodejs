7408020eb46f4a985a83c697afd4578f53f36bcd
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-29 17:41:52 +0530

    Refactor server setup to handle dynamic port assignment and improve error handling

--- Learn_Node/mini_project/url/app.js
+++ Learn_Node/mini_project/url/app.js
@@ -2,12 +2,13 @@ import { createServer } from "http";
 import fs from "fs/promises";
 import path from "path";
 
-const PORT = 3000;
+let PORT = process.env.PORT || 3000;
+const MAX_PORT = PORT + 100; // We'll try up to 100 ports
 
-const servFile = (res, filePath, ContentType) => {
+const servFile = async (res, filePath, contentType) => {
   try {
-    const data = fs.readFile(filePath);
-    res.writeHead(200, { "Content-Type": ContentType });
+    const data = await fs.readFile(filePath);
+    res.writeHead(200, { "Content-Type": contentType });
     res.end(data);
   } catch (error) {
     res.writeHead(404, { "Content-Type": "text/plain" });
@@ -18,9 +19,12 @@ const servFile = (res, filePath, ContentType) => {
 const server = createServer(async (req, res) => {
   if (req.method === "GET") {
     if (req.url === "/") {
-      return servFile(res, path.join("public", "index.html"), "text/html");
+      await servFile(res, path.join("public", "index.html"), "text/html");
     } else if (req.url === "/style.css") {
-      return servFile(res, path.join("public", "style.css"), "text/css");
+      await servFile(res, path.join("public", "style.css"), "text/css");
+    } else {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("404 Page Not Found");
     }
   } else {
     res.writeHead(405, { "Content-Type": "text/plain" });
@@ -28,6 +32,29 @@ const server = createServer(async (req, res) => {
   }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const startServer = () => {
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+  server.on("error", (e) => {
+    if (e.code === "EADDRINUSE") {
+      console.log(`Port ${PORT} is in use, trying the next one...`);
+      PORT++;
+      if (PORT <= MAX_PORT) {
+        server.close();
+        startServer();
+      } else {
+        console.error(
+          "Unable to find an open port. Please close some applications and try again."
+        );
+        process.exit(1);
+      }
+    } else {
+      console.error("An error occurred:", e.message);
+      process.exit(1);
+    }
+  });
+};
+
+startServer();
