3d64bb5799a1d3dbc14a88cbc6fdf9504d049c79
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-01-01 12:10:53 +0530

    Add contact, about, and services pages with corresponding routes

--- Learn_Node/express/app.js
+++ Learn_Node/express/app.js
@@ -3,10 +3,44 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  return res.send(`<h1>This is the home page</h1>`);
 });
 
-const PORT = 3000;
+app.get("/contact", (req, res) => {
+  return res.send(`<div class="container">
+      <h1>URL Shortener</h1>
+      <form id="shorten-form">
+        <label for="url">Enter your URL:</label>
+        <input
+          type="url"
+          name="url"
+          id="url"
+          placeholder="Enter your URL here"
+          required
+        />
+        <label for="short-url">Short Code :</label>
+        <input
+          type="text"
+          name="shortCode"
+          id="shortCode"
+          placeholder="Enter your short code here"
+        />
+        <button type="submit">Shorten</button>
+        <h2>Shortened URLs</h2>
+        <ul id="shortened-urls"></ul>
+      </form>
+    </div>`);
+});
+
+app.get("/about", (req, res) => {
+  return res.send(`<h1>This is the about page</h1>`);
+});
+
+app.get("/services", (req, res) => {
+  return res.send(`<h1>This is the services page</h1><p>Hey</p><p>Hey</p>`);
+});
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
