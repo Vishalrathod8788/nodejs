7bd39a2bf992ef2ea2dc617671457355f5005d23
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-01-01 17:33:50 +0530

    Refactor app.js to import PORT from env.js and update contact route response; add zod dependency for schema validation

--- Learn_Node/express/app.js
+++ Learn_Node/express/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import { PORT } from "./env.js";
 const app = express();
 
 app.get("/", (req, res) => {
@@ -7,29 +7,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/contact", (req, res) => {
-  return res.send(`<div class="container">
-      <h1>URL Shortener</h1>
-      <form id="shorten-form">
-        <label for="url">Enter your URL:</label>
-        <input
-          type="url"
-          name="url"
-          id="url"
-          placeholder="Enter your URL here"
-          required
-        />
-        <label for="short-url">Short Code :</label>
-        <input
-          type="text"
-          name="shortCode"
-          id="shortCode"
-          placeholder="Enter your short code here"
-        />
-        <button type="submit">Shorten</button>
-        <h2>Shortened URLs</h2>
-        <ul id="shortened-urls"></ul>
-      </form>
-    </div>`);
+  res.send("<h1>This is the contact page</h1>");
 });
 
 app.get("/about", (req, res) => {
@@ -40,7 +18,6 @@ app.get("/services", (req, res) => {
   return res.send(`<h1>This is the services page</p>`);
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
---
+++ Learn_Node/express/env.js
@@ -0,0 +1,26 @@
+export const PORT = isNaN(process.env.PORT) ? 3000 : parseInt(process.env.PORT);
+
+// what is zod libarary ?
+// zod is a TypeScript-first schema declaration and validation library.
+// It is designed to be as simple as possible, and it is built on top of TypeScript's type system,
+// which makes it easy to use and understand.
+
+import { z, ZodError } from "zod";
+
+const ageSchema = z.number().min(18).max(100).int().positive();
+
+const userAge = 17;
+
+// const parseUserAge = ageSchema.parse(userAge);
+// console.log(parseUserAge);
+
+try {
+  const parseUserAge = ageSchema.parse(userAge);
+  console.log(parseUserAge);
+} catch (error) {
+  if (error instanceof ZodError) {
+    console.log(error.issues[0].message);
+  } else {
+    console.log(error);
+  }
+}
