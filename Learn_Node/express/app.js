fd9ba70c8f8933a8684fe88a824794ac3d8bad4d
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-01-02 23:57:23 +0530

    Add static file serving and enhance index.html for personal portfolio

--- Learn_Node/express/app.js
+++ Learn_Node/express/app.js
@@ -1,9 +1,17 @@
 import express from "express";
 import { PORT } from "./env.js";
+import path from "path";
 const app = express();
 
+// mera tarika
+const staticPath = path.join(import.meta.dirname, "public");
+app.use(express.static(staticPath));
+
+// sir ka tarika
+// app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  return res.send(`<h1>This is the home page</h1>`);
+  res.send("<h1>This is the home page</h1>");
 });
 
 app.get("/contact", (req, res) => {
