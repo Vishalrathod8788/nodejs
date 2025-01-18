4232c5aeec870e158a77e75e3fc167bd1f44095a
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-01-18 12:10:02 +0530

    Remove unused route handlers for home, contact, about, and services pages in app.js

--- Learn_Node/express/app.js
+++ Learn_Node/express/app.js
@@ -15,22 +15,6 @@ const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
 const data = await response.json();
 console.log(data);
 
-app.get("/", (req, res) => {
-  res.send("<h1>This is the home page</h1>");
-});
-
-app.get("/contact", (req, res) => {
-  res.send("<h1>This is the contact page</h1>");
-});
-
-app.get("/about", (req, res) => {
-  return res.send(`<h1>This is the about page</h1>`);
-});
-
-app.get("/services", (req, res) => {
-  return res.send(`<h1>This is the services page</p>`);
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
