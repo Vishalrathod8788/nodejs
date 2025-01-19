2fb4fa1d36f5c2093d5a98b7e32b98232785c5e9
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-01-19 16:49:33 +0530

    Refactor contact route in app.js to handle user input; update contact form in contact.html to use structured input names and improve styling

--- Learn_Node/express/app.js
+++ Learn_Node/express/app.js
@@ -10,27 +10,13 @@ app.use(express.static(staticPath));
 // sir ka tarika
 // app.use(express.static("public"));
 
-app.get("/profile/:username", (req, res) => {
-  // console.log(req.params);
-  res.send(`<h1>My Name is ${req.params.username}</h1>`);
-});
-
-app.get("/profile/:username/article/:slug", (req, res) => {
-  const formatedSlug = req.params.slug.replace(/-/g, " ");
-  res.send(
-    `<h1>Article is ${formatedSlug} Created by ${req.params.username}</h1>`
-  );
-});
-
-app.get("/product", (req, res) => {
+app.get("/contact", (req, res) => {
   console.log(req.query);
-  res.send(`<h1>User Search Page No. in  ${req.query.search} </h1>`);
+  res.send(`<h1>User Name : ${req.query.name}</h1>`);
+  res.send(`<h1>User Email : ${req.query.email}</h1>`);
+  res.send(`<h1>User Message : ${req.query.message}</h1>`);
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
-
-app.get("/contact", (req, res) => {
-  res.sendFile(path.join(staticPath, "contact.html"));
 });
