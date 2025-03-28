ae0e763f77034f815c3b3b9a0a8d507132c0b30a
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-03-28 16:46:25 +0530

    Implement custom event handling and tracking in demo.js; remove eventCount.json

--- Learn_Node/event/demo.js
+++ Learn_Node/event/demo.js
@@ -1,16 +1,56 @@
-const event = require("events");
+const emitter = require("events");
+const fs = require("fs");
+const path = require("path");
+const fileName = "eventCount.json";
 
-const handleFunction = (myName) => {
-  console.log(`Hello my name is ${myName}, this is handleFunction`);
+const filePath = path.join(__dirname, fileName);
+
+// create at least four custom events (e. g., user-login, user-logout, user-purchase, profile-update)
+// Emit this event multiple time with diffrent argument (e. g., username, item purchased).
+// Treack and store the count of each event type:
+// Define summury event that logs a detailed report of how many time each event was triggered.
+
+const myEmitter = new emitter();
+let eventCounter = {
+  "user-login": 0,
+  "user-logout": 0,
+  "user-purchased": 0,
+  "profile-update": 0,
 };
 
-// Create Instence of Emitter
-const emitter = new event.EventEmitter();
-
-// 1. Define an event lister (addListener)
-emitter.on("myFun", (myName) => {
-  handleFunction(myName);
+myEmitter.on("user-login", (username) => {
+  console.log(
+    `${username} wants to login, please and ${eventCounter["user-login"]}`
+  );
+  eventCounter["user-login"]++;
+  fs.writeFileSync(filePath, JSON.stringyfy(eventCounter));
 });
 
-// 2. Trigger an event (emit)
-emitter.emit("myFun", "Vishal");
+myEmitter.on("user-logout", (username) => {
+  console.log(`${username} wants to logout, please`);
+  eventCounter["user-logout"]++;
+  fs.writeFi
+});
+
+myEmitter.on("user-purchased", (product) => {
+  console.log(`${product} purchaed !`);
+  eventCounter["user-purchased"]++;
+});
+
+myEmitter.on("profile-update", (email) => {
+  console.log(`${email} updated !`);
+  eventCounter["profile-update"]++;
+});
+
+myEmitter.on("summry", () => {
+  console.log(eventCounter);
+});
+
+myEmitter.emit("user-login", "Vishal");
+myEmitter.emit("user-logout", "Sahil");
+myEmitter.emit("user-purchased", "Laptop");
+myEmitter.emit("profile-update", "vishal8788@gmail.com");
+
+myEmitter.emit("summry", () => {
+  console.log(eventCounter);
+});
