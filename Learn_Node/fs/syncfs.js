f55a1cbe5a9477aa81e2fd8e675cdb3cf62ded06
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-03-28 11:27:55 +0530

    Refactor file handling in syncfs.js and update package.json to use CommonJS

--- Learn_Node/fs/syncfs.js
+++ Learn_Node/fs/syncfs.js
@@ -1,31 +1,22 @@
-// utf-8 (8-bit Unicode Transformation Format) --> Encode character from nearly all written language
 const fs = require("fs");
+const path = require("path");
 
-const fileName = "newTest.txt";
+// Create a file
+const fileName = "Test.txt";
+const filePath = path.join(__dirname, fileName);
 
-const writefile = fs.writeFileSync(
-  fileName,
-  "This is the initial comment",
-  "utf-8"
+// Sync
+const data = fs.writeFileSync(filePath, "Hello, World!", "utf8");
+console.log(data);
+
+// Async
+const writeData = fs.writeFile(
+  "AsyncTest.txt",
+  "Hello, This is Async File",
+  "utf8",
+  (err) => {
+    console.log(err);
+  }
 );
 
-console.log(writefile);
-
-// Read File
-
-const readfile = fs.readFileSync(fileName, "utf-8");
-console.log("Read :", readfile);
-
-// Update File
-const updateFile = fs.appendFileSync(fileName, "This is the updated comment");
-console.log(updateFile);
-const readfile2 = fs.readFileSync(fileName, "utf-8");
-console.log("Update :", readfile2);
-
-fs.unlink
-const deleteFile = fs.unlinkSync(fileName);
-console.log(deleteFile);
-
-// fs.renameSync
-const renameFile = fs.renameSync(fileName, "newTest.txt");
-console.log(renameFile);
+const readData = fs.readFile();
