f66e64ee3a7dce4eb8679e11846bd1d6090ca937
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-05-10 16:29:01 +0530

    Add dotenv for environment variable management and update app.js to use it

--- Learn_Node/new-express/app.js
+++ Learn_Node/new-express/app.js
@@ -1,12 +1,23 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 // Create Middleware 
 app.get('/', (req, res) => {
     return res.send("Hello World");
 })
 
-app.listen(import.meta.PORT, () => {
+
+app.get('/about', (req, res) => {
+    return res.send("About Page");
+})
+
+
+
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
