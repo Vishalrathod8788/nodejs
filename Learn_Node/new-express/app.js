d941767ff43b83bc3930e9c643ddb6dbe74b44e1
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-07-13 17:49:16 +0530

    Structure Formated

--- Learn_Node/new-express/app.js
+++ Learn_Node/new-express/app.js
@@ -1,49 +1,52 @@
-import express from 'express';
-import path from 'path';
-import dotenv from 'dotenv';
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-const staticPath = path.join(import.meta.dirname, 'public');
+const staticPath = path.join(import.meta.dirname, "public");
 
 app.use(express.static(staticPath));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(import.meta.dirname, 'views', '404.html'));
-})
+  res.status(404).sendFile(path.join(import.meta.dirname, "views", "404.html"));
+});
 
 // without using async/await
-const response = await fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json()).catch(err => console.error('Error fetching data:', err));
+const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((res) => res.json())
+  .catch((err) => console.error("Error fetching data:", err));
 console.log(response[99].userId);
 
-
-app.get('/', (req, res) => {
-    const homePagePath = path.join(import.meta.dirname, 'public', 'index.html');
-    res.sendFile(homePagePath);
-})
+app.get("/", (req, res) => {
+  const homePagePath = path.join(import.meta.dirname, "public", "index.html");
+  res.sendFile(homePagePath);
+});
 
 // Route parameter
-app.get('/user/:username', (req, res) => {
-    const username = req.params.username;
-    // console.log(req.params);
-    res.send(`Hello, My name is ${username}`);
-})
+app.get("/user/:username", (req, res) => {
+  const username = req.params.username;
+  // console.log(req.params);
+  res.send(`Hello, My name is ${username}`);
+});
 
 // Query parameter
-app.get('/product', (req, res) => {
-    console.log(req.query);
-    res.send(`The product name is ${req.query.product} and the price is ${req.query.price}`);
-})
+app.get("/product", (req, res) => {
+  console.log(req.query);
+  res.send(
+    `The product name is ${req.query.product} and the price is ${req.query.price}`
+  );
+});
 
 // contact route
-app.post('/contact', (req, res) => {
-    console.log(req.body);
-    // res.redirect(`https://www.google.com/search?q=${req.body.message}`);
-    res.send(`The message is ${req.body.user.message}`);
-})
+app.post("/contact", (req, res) => {
+  console.log(req.body);
+  // res.redirect(`https://www.google.com/search?q=${req.body.message}`);
+  res.send(`The message is ${req.body.user.message}`);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running ${`http://localhost:${PORT}`}`);
-})
+  console.log(`Server is running ${`http://localhost:${PORT}`}`);
+});
