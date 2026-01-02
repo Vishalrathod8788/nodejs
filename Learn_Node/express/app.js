db9d429072cccfa5dbf0ceaa6f05892bf05250ef
Vishal Rathod <vishalrathod1562004@gmail.com>
2026-01-02 11:58:30 +0530

    feat: Enhance Express app with dynamic routing and contact form functionality

--- Learn_Node/express/app.js
+++ Learn_Node/express/app.js
@@ -1,9 +1,17 @@
 import { PORT } from "./env.js";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome to Express server!!");
+  const homePage = path.join(__dirname, "public", "index.html");
+  res.status(200).sendFile(homePage);
 });
 
 app.post("/", (req, res) => {
@@ -16,6 +24,41 @@ app.get("/test", (req, res) => {
 
 app.get("/status", (req, res) => {
   res.status(200).send("Express Server is up and running!!");
+});
+
+//get Data from dynamic URL
+app.get("/user/:id", (req, res) => {
+  res.send(`<h1>Hello My user name is ${req.params.id}</h1>`);
+});
+
+app.get("/blog/:id", (req, res) => {
+  res.send(`<h1> this blog is about ${req.params.id}`);
+});
+
+app.get("/user/:username/blog/:sulg", (req, res) => {
+  const formatedSlug = req.params.sulg.replace(/-/g, " ");
+  res.send(
+    `<h1>This blog about ${formatedSlug} and this blog created by ${req.params.username}</h1>`
+  );
+});
+
+// get Data from Query Parameters
+app.get("/product", (req, res) => {
+  console.log(req.query);
+  res.send(`User search product is ${req.query.search}`);
+});
+
+// Handle Form Submission in Express
+// GET method
+// app.get("/contact", (req, res) => {
+//   console.log(req.query);
+//   res.send("Form submited successfully!!");
+// });
+
+// Post Method
+app.post("/contact", (req, res) => {
+  console.log(req.body);
+  res.send("Form Submited Successfully!!");
 });
 
 app.listen(PORT, () => {
--- Learn_Node/express/env.js
+++ Learn_Node/express/env.js
@@ -1,11 +1,1 @@
-// export const PORT = isNaN(process.env.PORT) ? 3000 : parseInt(process.env.PORT);
-
-import { z } from "zod";
-
-const ageSchema = z.number().min(18).max(100).int();
-
-const userAge = 16;
-
-const parseUserAge = ageSchema.parse(userAge);
-
-console.log(parseUserAge);
+export const PORT = isNaN(process.env.PORT) ? 3000 : parseInt(process.env.PORT);
